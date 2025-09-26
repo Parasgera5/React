0c8e241a13d9b1f23c944abb3edb710c4873df9f
Paras Gera <parasgera2005@gmail.com>
2025-09-26 16:38:01 +0530

    Building start

--- 22(newsApp)/news_app/src/App.js
+++ 22(newsApp)/news_app/src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
 
 import React, { Component } from 'react'
+import Navbar from './components/Navbar';
+import News from './components/News';
 
 export default class App extends Component {
   c = "Hello hye bye bye"
   render() {
     return (
-      <div>App: {this.c}</div>
+      // <>
+      //   <div>App: {this.c}</div>
+      //   <div>tere nain ne sharab diya do botla</div>
+      // </>
+      <div>
+        <Navbar/>
+        <News/>
+      </div>
     )
   }
 }
